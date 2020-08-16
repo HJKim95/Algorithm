674d3201e584562cdaa0db9429fd459917d5342f
HJKim95 <25ephipany@naver.com>
2020-08-16 16:32:39 +0900

    updated 0816

---
+++ Algorithm_ex/main.swift
@@ -0,0 +1,39 @@
+//
+//  main.swift
+//  Algorithm_ex
+//
+//  Created by 김희중 on 2020/08/16.
+//  Copyright © 2020 HJ. All rights reserved.
+//
+
+import Foundation
+
+//MARK: greedy1_p.87
+/*
+let readLines = readLine()!
+var returns = Int(readLines)!
+
+var resultArr = [Int]()
+resultArr.append(returns/500)
+
+returns -= (resultArr[0] * 500)
+
+resultArr.append(returns/100)
+
+returns -= (resultArr[1] * 100)
+
+resultArr.append(returns/50)
+
+returns -= (resultArr[2] * 50)
+
+resultArr.append((returns/10))
+
+print("거스름돈 결과")
+print("500원: ", resultArr[0])
+print("100원: ", resultArr[1])
+print("50원: ", resultArr[2])
+print("10원: ", resultArr[3])
+ */
+
+//MARK: greedy2_p.
+
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import algorithmTestTests
-
-var tests = [XCTestCaseEntry]()
-tests += algorithmTestTests.allTests()
-XCTMain(tests)
