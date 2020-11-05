bbd55167ddc1dbeda9a1e1952101bc5307497dd9
HJKim95 <25ephipany@naver.com>
2020-11-05 15:47:39 +0900

    updated 1105

---
+++ 백준/9251.swift
@@ -0,0 +1,32 @@
+//
+//  9251.swift
+//  Algorithm_ex
+//
+//  Created by 김희중 on 2020/11/05.
+//  Copyright © 2020 HJ. All rights reserved.
+//
+
+import Foundation
+//ACAYKP
+//CAPCAK
+var n = Array(readLine()!)
+var m = Array(readLine()!)
+
+n.insert(contentsOf: " ", at: 0)
+m.insert(contentsOf: " ", at: 0)
+
+var max_v = 0
+var arr = Array(repeating: Array(repeating: 0, count: n.count), count: m.count)
+for i in 1..<m.count {
+    for j in 1..<n.count {
+        if n[j] == m[i] {
+            arr[i][j] = arr[i-1][j-1] + 1
+        }
+        else {
+            arr[i][j] = max(arr[i][j-1], arr[i-1][j])
+        }
+        max_v = max(max_v,arr[i][j])
+    }
+}
+
+print(max_v)
